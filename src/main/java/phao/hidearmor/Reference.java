e97cdb30be4eacfc7dccd1bb938e74fbb7fe6c48
phaonica <phaonica@gmail.com>
2023-05-12 21:07:27 -0500

    base commit, includes basic mod setup from harry's tutorials

---
+++ src/main/java/phao/hidearmor/Reference.java
@@ -0,0 +1,12 @@
+package phao.hidearmor;
+
+public class Reference 
+{
+	public static final String MODID = "phaohide";
+	public static final String NAME  = "Hide Armor";
+	public static final String VERSION = "1.0 - MC 1.12.2";
+	
+	public static final String CLIENTPROXY = "phao.hidearmor.ClientProxy";
+	public static final String COMMONPROXY = "phao.hidearmor.CommonProxy";
+
+}
---
+++ src/main/java/phao/hidearmor/RegistryHandler.java
@@ -0,0 +1,14 @@
+package phao.hidearmor;
+
+public class RegistryHandler 
+{
+	public static void Client()
+	{
+		
+	}
+	
+	public static void Common()
+	{
+		
+	}
+}
