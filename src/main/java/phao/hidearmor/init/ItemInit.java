a31a646b2007b8f8897a9f7ee6af1b917147fe81
phaonica <phaonica@gmail.com>
2023-05-14 23:29:12 -0500

    hide armor updated resources and recipes

--- src/main/java/phao/hidearmor/init/ItemInit.java
+++ src/main/java/phao/hidearmor/init/ItemInit.java
@@ -31,7 +31,7 @@ public class ItemInit
 
 	
 	/*
-	 *         LEATHER("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F),
+	 *  LEATHER("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F),
         CHAIN("chainmail", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F),
         IRON("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F),
         GOLD("gold", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F),
@@ -40,12 +40,16 @@ public class ItemInit
 	 */
 	
 	//public static final ArmorMaterial ARMOR_COPPER = EnumHelper.addArmorMaterial(name, textureName, durability, reductionAmounts, enchantability, soundOnEquip, toughness)
-	public static final ArmorMaterial ARMOR_TUTORIAL = EnumHelper.addArmorMaterial("armor_tutorial", Reference.MODID+":tutorial", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F); // leather values
+	public static final ArmorMaterial ARMOR_TUTORIAL = EnumHelper.addArmorMaterial("armor_tutorial", Reference.MODID+":tutorial", 3, new int[]{1, 1, 1, 1}, 5, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F); // below leather values
 	
 	public static final Item INGOT_TUTORIAL = new ItemBase("ingot_tutorial");
 	
 	public static final Item PICKAXE_TUTORIAL = new ToolPickaxe("pickaxe_tutorial", TOOL_TUTORIAL);
 
-	public static final Item HELMET_TUTORIAL = new ArmorBase("helmet_tutorial", ARMOR_TUTORIAL, 1, EntityEquipmentSlot.HEAD);
+	//public static final Item HELMET_TUTORIAL = new ArmorBase(name, material, renderIndex, slot);
+	public static final Item HELMET_TUTORIAL = new ArmorBase("armor_tutorial_head", ARMOR_TUTORIAL, 1, EntityEquipmentSlot.HEAD);
+	public static final Item CHEST_TUTORIAL = new ArmorBase("armor_tutorial_body", ARMOR_TUTORIAL, 1, EntityEquipmentSlot.CHEST);
+	public static final Item LEGGINGS_TUTORIAL = new ArmorBase("armor_tutorial_legs", ARMOR_TUTORIAL, 2, EntityEquipmentSlot.LEGS);
+	public static final Item BOOTS_TUTORIAL = new ArmorBase("armor_tutorial_feet", ARMOR_TUTORIAL, 1, EntityEquipmentSlot.FEET);
 
 }
